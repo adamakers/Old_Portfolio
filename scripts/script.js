1bbf37ca2cddf6820b8c31743b82584cd517d344
adamakers <adamrakers@gmail.com>
2017-03-20 20:52:55 -0600

    added item to skill: AJAX

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
-var boxHeader = document.querySelectorAll('.project-box-header');
+const projectBox = document.querySelectorAll('.project-box');
 
 
+projectBox.forEach(function(box){
+  box.addEventListener('click', function(e){
+    let boxSummary = box.querySelector('.project-box-summary');
+    let target = e.target;
 
-boxHeader.forEach(function(box){
-  box.addEventListener('click', function(){
-    var summary = document.querySelector('.project-box-summary');
-    summary.classList.toggle('box-open');
+    //onsole.log(target);
+    if (target && target.nodeName === 'I') {
+      boxSummary.classList.toggle('box-open');
+    }
+    // var summary = document.querySelector('.project-box-summary');
+    // summary.classList.toggle('box-open');
   });
 })
+
+
 
