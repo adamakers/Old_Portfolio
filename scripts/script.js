ac9c9d3b462de29ffd78f07a6fd0a65eb4b255c3
adamakers <adamrakers@gmail.com>
2017-04-25 21:00:03 -0600

    replaced lorem ipsum in about me section.

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,39 @@
 
 $(document).ready(function(){
-
   //lock navigation when scrolling
-  $(function() {
+
     var $window = $(window);
     var $mainNav = $('.main-nav');
     var distance = $mainNav.offset().top;
+
+      //smooth scroll
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
     
-    $window.scroll(function(){
+    function scrollEvent(){
       if ($window.scrollTop() >= distance) {
         $mainNav.addClass('locked');
       } else {
         $mainNav.removeClass('locked');
       }
-    });
-  });
+    }
 
-  //smooth scroll
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
+    $window.scroll(scrollEvent);
+
+    scrollEvent();
+
 
 });
 
